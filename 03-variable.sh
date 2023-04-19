3488121c2afddfc5301a8d7a6041927e0f979ac6
Raghu K <raghuk.vit@gmail.com>
2023-04-19 06:22:28 +0530

    Source Common

--- 03-variable.sh
+++ 03-variable.sh
@@ -14,4 +14,10 @@ ARTH=$((2-3*4/2))
 
 echo ARTH = ${ARTH}
 
+# Special Variables for Inputs
 echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Argument - $*
+echo No of Arguments - $#
+
