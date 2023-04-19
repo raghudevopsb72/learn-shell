a40807ddbd3a742f72814930b9e281fba39765ec
Raghu K <raghuk.vit@gmail.com>
2023-04-19 06:44:57 +0530

    Source Common

--- 04-functions.sh
+++ 04-functions.sh
@@ -9,3 +9,13 @@ function_name() {
 # Call function
 function_name
 
+## We can send inputs to the function and we can access them with special variables $1-$n, $*, $#
+
+function_name1() {
+  echo First Argument = $1
+  echo Second Argument = $2
+  echo All Arguments = $*
+  echo No Of Arguments = $#
+}
+
+function_name1 123 xyz
