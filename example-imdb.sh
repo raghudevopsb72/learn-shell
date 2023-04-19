1983be0634553fa5488c0002195e16563c18d9e8
Raghu K <raghuk.vit@gmail.com>
2023-04-19 07:26:48 +0530

    Source Common

---
+++ example-imdb.sh
@@ -0,0 +1,17 @@
+# Get rating
+# > 7 - Good
+# < 7 - Average
+
+input=$1
+if [ -z "$input" ]; then
+  echo Input Movie Name Missing
+  exit
+fi
+
+percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}')
+
+if [ "$percent" -ge 70 ]; then
+  echo Good
+else
+  echo Average
+fi
