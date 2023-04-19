895964671489934ff1e53b75ee3310c7d60a7814
Raghu K <raghuk.vit@gmail.com>
2023-04-19 07:28:35 +0530

    Source Common

--- example-imdb.sh
+++ example-imdb.sh
@@ -8,7 +8,7 @@ if [ -z "$input" ]; then
   exit
 fi
 
-percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}')
+percent=$(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
 echo Percent - $percent
 
