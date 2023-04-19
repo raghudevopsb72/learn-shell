f67efd87a443a71553f380bd59eb85e11437386f
Raghu K <raghuk.vit@gmail.com>
2023-04-19 07:03:30 +0530

    Source Common

--- 05-if.sh
+++ 05-if.sh
@@ -1,10 +1,11 @@
 fruit_name=$1
 quantity=$2
 
-if [ $fruit_name == "mango" ]
+if [ "$fruit_name" == "mango" ]
 then
   echo Mango Quantity - $quantity
 else
   echo Fruit does not exist
 fi
 
+## Always double quote variables in expressions
