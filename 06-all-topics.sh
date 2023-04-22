d93c771f7485154d3b7aad0963f4cee34887e046
Raghu K <raghuk.vit@gmail.com>
2023-04-22 06:22:17 +0530

    All Topics.

--- 06-all-topics.sh
+++ 06-all-topics.sh
@@ -21,6 +21,43 @@ echo -e "Line1\nLine2\n\nLine3"
 echo -e "word1\t\tword2"
 
 #3. Variables
+
+#Declare a variable
+a=10
+
+# Access a variable , Using echo command to print data in a variable
+echo  a - $a
+
+# Command Substitution
+DATE=$(date)
+echo Date - $DATE
+
+# Arithmetic Substitution
+ADD=$((2+3))
+echo Add of 2+3 - $ADD
+
+# Properties of a variable
+
+# Read write
+b=10
+b=20
+
+## b was declared with 10 value and again b was declared with 20 value
+echo b - $b
+
+# We will see 20, because the property read-write will allow to overwrite the value 10 in b with 20.
+
+# Sometimes in case if we need to mark a variable as readonly after declaring then a readonly command will help
+readonly b
+b=30
+
+# Local Variable
+# Variable declared in this script can be access within this script, because the default property is local.
+# variable declared in cli in shell cannot be accessed in script
+
+echo c - $c
+
+
 #4. Inputs
 #5. Conditions
 #6. Loop
