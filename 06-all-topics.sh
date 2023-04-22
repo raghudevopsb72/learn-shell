35c0a0b2e543373df69f24b6ed7e76edc346db08
Raghu K <raghuk.vit@gmail.com>
2023-04-22 06:12:26 +0530

    All Topics.

---
+++ 06-all-topics.sh
@@ -0,0 +1,27 @@
+# Comment  - Any line starting with # is a comment and shell will ignore those lines.
+
+#1. How to write shell script file
+
+# File ending with .sh is what we prefer to make a shell script so we understand that is a shell script. But for OS it does not matter.
+
+# how to run a shell script
+# bash script.sh
+
+#2. Printing
+
+# echo Word
+# Word will be printed on screen by echo command
+# echo We can print colors.
+## 02-echo.sh is already having those
+
+# We can print multiple lines in echo command
+echo -e "Line1\nLine2\nLine3"
+
+#3. Variables
+#4. Inputs
+#5. Conditions
+#6. Loop
+#7. Function
+#8. Exit Status
+#9. Quotes
+#10. SED Editor
