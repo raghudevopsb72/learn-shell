eaf38684dc17f63499143f905b4805e675ccc853
Raghu K <raghuk.vit@gmail.com>
2023-04-22 07:08:53 +0530

    All Topics.

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,3 +1,3 @@
 for component in frontend mongodb catalogue ; do
-  aws ec2 run-instances --image-id ami-0089b8e98cd95257d --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}]"
+  aws ec2 run-instances --image-id ami-0089b8e98cd95257d --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 done
